a3bd6d73753517804894245690ecc22510512dc4
anddres-developer <andresdlg1404@gmail.com>
2025-06-09 23:07:40 -0400

    añade la funcion de importar base de datos

--- vistas/paginas/respaldo.php
+++ vistas/paginas/respaldo.php
@@ -11,33 +11,35 @@
                 <h5 class="m-0 font-weight-bold text-primary">Importar Base de Satos</h5>
             </div>
             <div class="card-body">
-            <form method="POST" action="respaldo/importar_db.php" enctype="multipart/form-data">
-                <div class="input-group">
-                <div class="custom-file mr-1">
-                    <input type="file" class="custom-file-input" id="importarDB" aria-describedby="importar">
-                    <label class="custom-file-label" for="inputGroupFile04">Seleccione Archivo .sql</label>
-                </div>
-                <div class="input-group-append">
-                    <button class="btn btn-warning btn-icon-split" type="button" id="importar">
-                        <span class="icon text-white-50"><i class="fas fa-exclamation-triangle"></i></span> 
-                        <span class="text">Importar Base de datos</span></button>
-                </div>
-                </div>
+                <form method="POST" action="respaldo/importar_db.php" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-xl-8 col-lg-6 mb-2">
+                            <div class="custom-file mr-1">
+                                <input type="file" class="custom-file-input" name="archivo_sql" id="archivo_sql" accept=".sql" lang="es" required>
+                                <label class="custom-file-label" for="archivo_sql" id="archivo">Archivo.sql</label>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-6 text-center">
+                            <button class="btn btn-warning btn-icon-split" type="submit">
+                                <span class="icon text-white-50"><i class="fas fa-exclamation-triangle"></i></span>
+                                <span class="text">Importar Base de datos</span></button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
 
     <div class="col-lg-6">
-    <!-- Basic Card Example -->
-    <div class="card shadow mb-4 border-bottom-success">
+        <!-- Basic Card Example -->
+        <div class="card shadow mb-4 border-bottom-success">
             <div class="card-header py-3">
                 <h5 class="m-0 font-weight-bold text-primary">Exportar Base de Datos</h5>
             </div>
-            <div class="card-body text-center">
+            <div class="card-body text-center mb-2">
                 <a href="respaldo/exportar_db.php" class="btn btn-success btn-icon-split">
                     <span class="icon text-white-50">
-                    <i class="fas fa-download"></i>
+                        <i class="fas fa-download"></i>
                     </span>
                     <span class="text">Exportar base de datos</span>
                 </a>
@@ -46,3 +48,13 @@
     </div>
 
 </div>
+
+<script>
+    const fileInput = document.getElementById('archivo_sql');
+    const fileLabel = document.getElementById('archivo');
+
+    fileInput.addEventListener('change', function() {
+        const fileName = this.value.split('\\').pop(); // Obtiene solo el nombre del archivo
+        fileLabel.textContent = fileName;
+    });
+</script>
